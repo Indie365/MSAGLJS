e0f7f7b6b19e496a2e5af64e152606f436252687
pelikhan <jhalleux@microsoft.com>
2022-05-24 18:00:04 -0700

    basic blocks

---
+++ examples/react/src/App.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/microsoft/msagljs">
+        Microsoft MSAGL JavaScript
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  )
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="xl">
+      <h1>MSGAL</h1>
+      <Copyright />
+    </Container>
+  )
+}
---
+++ examples/react/src/index.tsx
@@ -0,0 +1,17 @@
+import {createRoot} from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline'
+import {ThemeProvider} from '@mui/material/styles'
+import App from './App'
+import theme from './theme'
+
+const rootElement = document.getElementById('root')
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(rootElement!)
+
+root.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+)
