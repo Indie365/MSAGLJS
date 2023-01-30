83cecada822225b83b3f2b18e88b8148189cc204
Lev Nachmanson <levnach@hotmail.com>
2023-01-30 08:43:13 -1000

    fix the build_examples.py
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- build_examples.py
+++ build_examples.py
@@ -21,6 +21,7 @@ os.system(git_clone_cmd)
 
 os.chdir(enlisment_dir)
 os.system("/usr/bin/yarn")
+os.system("/usr/bin/yarn build")
 
 sources = ["webgl-renderer","svg-renderer" ]
 targets = ["deck.gl_backend","svg_backend" ]
@@ -30,7 +31,6 @@ targets = ["deck.gl_backend","svg_backend" ]
 for x in range (0,2) :
    os.chdir(os.path.join(deploy_dir, enlisment_dir, "examples",sources[x]))
    print("current " + os.getcwd())
-   os.system("npm run build") 
    target_path = os.path.join(deploy_dir, targets[x])
    shutil.rmtree(target_path)
    shutil.copytree("dist", target_path)
